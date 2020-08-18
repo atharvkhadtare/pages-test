28f21c43bbeb8eaf0e95a40a8aede2bca185eac4
atharvkhadtare <atharvkhadtare@gmail.com>
2020-08-18 19:01:49 +0530

    utils.py created, districtwise and statewise json created

---
+++ scripts/create_json_districtwise.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+from utils import to_json_file, progressBar, download_json_data
+import json
+from datetime import datetime, timedelta
+from collections import OrderedDict
+from os import makedirs, path, listdir
+import argparse
+from time import sleep
+
+version = '1.0.0'
+url = "https://api.covid19india.org/v4/data-YYYY-MM-DD.json"
+state_district_map = dict()
+state_parent_dir = '../data/json/districtwise/'
+def download_raw_daily_data():
+	force_refresh = 1
+	date = datetime.today()
+	makedirs("../data/raw/daily/")
+	print("Downloading raw daily data\nForce Refreshed:")
+	for i in progressBar(range(force_refresh)):
+		# sleep(0.2)
+		# print("FORCE: " + date.strftime("https://api.covid19india.org/v4/data-%Y-%m-%d.json"))
+		try:
+			data = download_json_data(date.strftime("https://api.covid19india.org/v4/data-%Y-%m-%d.json"))
+			to_json_file(data, date.strftime("../data/raw/daily/%Y-%m-%d.json"))
+		except:
+			pass
+		date = date - timedelta(days=1)
+
+	max_days = (date - datetime.strptime("2020-02-29", "%Y-%m-%d")).days
+	print("If not cached:")
+	for i in progressBar(range(max_days)):
+		# sleep(0.05)
+		if not path.isfile(date.strftime("../data/raw/daily/%Y-%m-%d.json")):
+			try:
+				# print("DOWNLOADING: " + date.strftime("https://api.covid19india.org/v4/data-%Y-%m-%d.json"))
+				data = download_json_data(date.strftime("https://api.covid19india.org/v4/data-%Y-%m-%d.json"))
+				to_json_file(data, date.strftime("../data/raw/daily/%Y-%m-%d.json"))
+				# break
+				pass
+			except:
+				pass
+		else:
+			# print("CACHED: " + date.strftime("https://api.covid19india.org/v4/data-%Y-%m-%d.json"))
+			pass
+		date = date - timedelta(days=1)
+	return 0
+
+def process_raw_daily_files():
+	file_list = sorted(listdir("../data/raw/daily/"))
+	print("Processing raw daily files")
+	for filename in progressBar(file_list):
+		if filename == "YYYY-MM-DD.json":
+			continue
+		# print(filename)
+		with open("../data/raw/daily/" + filename) as json_file:
+			json_data = json.load(json_file)
+			for state, values in json_data.items():
+				if state not in state_district_map:
+					# print("\tadd " + state)
+					state_district_map[state] = set()
+				if "districts" in values:
+					for district, values in values["districts"].items():
+						for count_type in ['total', 'delta']:
+							if count_type not in values:
+								values[count_type] = dict()
+							for status in ['confirmed', 'deceased', 'recovered']:
+								if status not in values[count_type]:
+									values[count_type][status] = 0
+							values[count_type]['active'] = values[count_type]['confirmed'] - values[count_type]['deceased'] - values[count_type]['recovered']
+						if district not in state_district_map[state]:
+							state_district_map[state].add(district)
+							open_daily_district_file(filename.split('.')[0], values, state, district)
+						else:
+							add_to_daily_district_file(
+								filename.split('.')[0], values, state, district)
+						json_filepath = '../data/json/districtwise/' + state + '/' + district + '/' + filename
+						to_json_file(values, json_filepath)
+	close_all_daily_district_files()
+	# print(state_district_map)
+	pass
+
+def open_daily_district_file(date, json_data, statecode, district):
+	district_parent_dir = '../data/json/districtwise/' + statecode + '/' + district
+	makedirs(district_parent_dir, exist_ok=True)
+	with open(district_parent_dir + '/daily.json', 'w', encoding='utf-8') as json_file:
+		json_file.write("{\n" + "\"" + date + "\": ")
+		json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+
+def add_to_daily_district_file(date, json_data, statecode, district):
+	district_parent_dir = '../data/json/districtwise/' + statecode + '/' + district
+	with open(district_parent_dir + '/daily.json', 'a', encoding='utf-8') as json_file:
+		json_file.write(",\n" + "\"" + date +"\": ")
+		json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+
+def close_all_daily_district_files():
+	state_list = sorted(listdir('../data/json/districtwise/'))
+	print("Closing daily district files")
+	for state in progressBar(state_list):
+		if state == 'STATE':
+			continue
+		district_list = sorted(listdir('../data/json/districtwise/' + state))
+		for district in district_list:
+			district_parent_dir = '../data/json/districtwise/' + state + '/' + district
+			with open(district_parent_dir + '/daily.json', 'a', encoding='utf-8') as json_file:
+				json_file.write("}")
+
+def main():
+	download_raw_daily_data()
+	process_raw_daily_files()
+	# data = download_json_data(url)
+	# print(data)
+	# statecodes = list(list(data.values())[0][0].keys())
+	# statecodes.remove('status')
+	# statecodes.remove('date')
+	# # for x in data.values():
+	# 	print(x)
+	# for statecode in progressBar(statecodes):
+	# 	create_json_statwise(data, statecode)
+
+if __name__ == "__main__":
+	main()
--- scripts/create_json_statewise.py
+++ scripts/create_json_statewise.py
@@ -1,68 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import requests
-import json
+from utils import to_json_file, progressBar, download_json_data
 from datetime import datetime
 from collections import OrderedDict
-import os
-from time import sleep
+from os import makedirs
+import argparse
 
-url = {}
+version = '1.0.0'
 url = "https://api.covid19india.org/states_daily.json"
-
-def progressBar(iterable, prefix='Progress: ', suffix='Complete', decimals=1, length=50, fill='█', printEnd="\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    total = len(iterable)
-    # Progress Bar Printing Function
-
-    def printProgressBar(iteration):
-        percent = ("{0:." + str(decimals) + "f}").format(100 *
-                                                         (iteration / float(total)))
-        filledLength = int(length * iteration // total)
-        bar = fill * filledLength + '-' * (length - filledLength)
-        print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end=printEnd)
-
-    # Initial Call
-    printProgressBar(0)
-    # Update Progress Bar
-    for i, item in enumerate(iterable):
-        yield item
-        printProgressBar(i + 1)
-    # Print New Line on Complete
-    print()
-
-def download_data(url):
-	r = requests.get(url)
-	return r.json()
 
 def create_json_statwise(data, statecode):
 	daily_json = {}
 	current = {}
 	state_parent_dir = '../data/json/statewise/'+statecode + '/'
-	os.makedirs(state_parent_dir, exist_ok=True)
-	current["Confirmed"] = 0
-	current["Recovered"] = 0
-	current["Deceased"] = 0
-	current["Active"] = 0
+	makedirs(state_parent_dir, exist_ok=True)
+	current["Confirmed"] = current["Recovered"] = current["Deceased"] = current["Active"] = 0
 	for record in data['states_daily']:
-		# daily_json[i["date"]]
 		record_date = datetime.strptime(record["date"], "%d-%b-%y").strftime("%Y-%m-%d")
 		if record_date not in daily_json:
 			daily_json[record_date] = {}
 			daily_json[record_date]["delta"] = {}
 		daily_json[record_date]["delta"][record["status"]] = int(record[statecode])
-		# print(i["date"] + "\t\t" + i["status"])
 	daily_json = OrderedDict(sorted(daily_json.items()))
 	for (date, values) in daily_json.items():
 		delta = values["delta"]
@@ -70,19 +28,24 @@ def create_json_statwise(data, statecode):
 		for status in ["Confirmed", "Recovered", "Deceased", "Active"]:
 			values[status] = current[status] + delta[status]
 			current[status] = values[status]
-		with open(state_parent_dir + date +'.json', 'w', encoding='utf-8') as f:
-			json.dump(values, f, ensure_ascii=False, indent=4)
-	with open('../data/json/statewise/'+statecode+'/daily.json', 'w', encoding='utf-8') as f:
-		json.dump(daily_json, f, ensure_ascii=False, indent=4)
+		to_json_file(values, state_parent_dir + date + '.json')
+	to_json_file(daily_json, state_parent_dir + 'daily.json')
 
-data = download_data(url)
-statecodes = list(list(data.values())[0][0].keys())
-statecodes.remove('status')
-statecodes.remove('date')
-# for x in data.values():
-# 	print(x)
-for statecode in progressBar(statecodes):
-	create_json_statwise(data, statecode)
-# for statecode in statecodes:
-# 	create_json_statwise(data, statecode)
-# print( date.date()) 
+def main():
+	parser = argparse.ArgumentParser(description='Download statewise stats for each day and create json files.', prog='COVID-stats')
+	parser.add_argument('-v', '--verbose', help='verbose', action='store_true')
+	# parser.add_argument('-p', '--progress_bar', help='show progress bar', action='store_true')
+	parser.add_argument('--version', action='version', version='%(prog)s ' + version)
+	# args = parser.parse_args()
+	data = download_json_data(url)
+	# print(data)
+	statecodes = list(list(data.values())[0][0].keys())
+	statecodes.remove('status')
+	statecodes.remove('date')
+	# for x in data.values():
+	# 	print(x)
+	for statecode in progressBar(statecodes):
+		create_json_statwise(data, statecode)
+
+if __name__ == "__main__":
+    main()
